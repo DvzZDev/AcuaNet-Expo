ba008c453fdb05a3ea0a6154d508b3aa63fae897
DvzZ <estebandavid578@gmail.com>
2025-05-12 07:11:45 +0200

    Add Inter font family to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Inter: ['Inter', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 };
