768578aeee1e311afddffe1c3ff7172094f1a41f
DvzZ <estebandavid578@gmail.com>
2025-06-15 20:00:38 +0200

    feat: Update app configuration by reordering plugins and adjusting icon paths

--- app.config.js
+++ app.config.js
@@ -3,18 +3,29 @@ export default {
     name: "AcuaNet",
     slug: "AcuaNet",
     version: "1.0.0",
-    web: {
-      favicon: "./assets/favicon.png",
-    },
+    orientation: "portrait",
+    userInterfaceStyle: "light",
+    scheme: "acuanet",
+    assetBundlePatterns: ["**/*"],
+
+    icon: "./assets/IosIcon.png",
+
     experiments: {
       tsconfigPaths: true,
     },
+
     plugins: [
       "expo-router",
       [
         "expo-font",
         {
           fonts: ["./assets/fonts/Inter.ttf"],
+        },
+      ],
+      [
+        "expo-image-picker",
+        {
+          photosPermission: "AcuaNet necesita acceso a tus fotos para que puedas subir imágenes.",
         },
       ],
       [
@@ -25,42 +36,35 @@ export default {
           backgroundColor: "#16151a",
         },
       ],
-      [
-        "expo-image-picker",
-        {
-          photosPermission: "AcuaNet necesita acceso a tus fotos para que puedas subir imágenes.",
-        },
-      ],
     ],
-    orientation: "portrait",
-    icon: "./assets/Icon-Ios.png",
-    userInterfaceStyle: "light",
-    assetBundlePatterns: ["**/*"],
+
     ios: {
-      supportsTablet: false,
       bundleIdentifier: "com.dvzz.AcuaNet",
+      supportsTablet: false,
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
       },
     },
-    scheme: "acuanet",
+
     android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/Icon-Android.png",
-      },
       package: "com.dvzz.AcuaNet",
+      adaptiveIcon: {
+        foregroundImage: "./assets/AndroidIcon.png",
+        backgroundImage: "./assets/Android-Icon-Bg.png",
+      },
       config: {
         googleMaps: {
           apiKey: process.env.EXPO_GOOGLE_MAPS_API_KEY,
         },
       },
     },
+
     extra: {
-      router: {},
       eas: {
         projectId: "cbd687c4-483a-4df3-b443-94debaf44880",
       },
       googleMapsApiKey: process.env.EXPO_GOOGLE_MAPS_API_KEY,
+      router: {},
     },
   },
 }
