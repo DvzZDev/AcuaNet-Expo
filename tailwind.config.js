456676155c50f91af5f93dce3e7056aade98026f
DvzZ <estebandavid578@gmail.com>
2025-07-05 14:16:27 +0200

    fix: update content paths in tailwind configuration for improved file matching

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+  content: [
+    "./screens/**/*.{js,ts,tsx}",
+    "./components/**/*.{js,ts,tsx}",
+    "./navigation/**/*.{js,ts,tsx}",
+    "./App/**/*.{js,ts,tsx}",
+    "./src/**/*.{js,ts,tsx}",
+    "./index.js",
+  ],
 
-  presets: [require('nativewind/preset')],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {
       fontFamily: {
-        'Inter-Black': ['Inter-Black', 'sans-serif'],
-        'Inter-Bold': ['Inter-Bold', 'sans-serif'],
-        'Inter-ExtraBold': ['Inter-ExtraBold', 'sans-serif'],
-        'Inter-Light': ['Inter-Light', 'sans-serif'],
-        'Inter-Medium': ['Inter-Medium', 'sans-serif'],
-        'Inter': ['Inter-Regular', 'sans-serif'],
-        'Inter-SemiBold': ['Inter-SemiBold', 'sans-serif'],
-        'Inter-Thin': ['Inter-Thin', 'sans-serif'],
-        'Inter-Black-Italic': ['Inter-Black-Italic', 'sans-serif'],
+        "Inter-Black": ["Inter-Black", "sans-serif"],
+        "Inter-Bold": ["Inter-Bold", "sans-serif"],
+        "Inter-ExtraBold": ["Inter-ExtraBold", "sans-serif"],
+        "Inter-Light": ["Inter-Light", "sans-serif"],
+        "Inter-Medium": ["Inter-Medium", "sans-serif"],
+        Inter: ["Inter-Regular", "sans-serif"],
+        "Inter-SemiBold": ["Inter-SemiBold", "sans-serif"],
+        "Inter-Thin": ["Inter-Thin", "sans-serif"],
+        "Inter-Black-Italic": ["Inter-Black-Italic", "sans-serif"],
       },
     },
   },
   plugins: [],
-};
+}
